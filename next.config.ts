4995a327720d50e1128657c35a387a437a3a554a
zoptero <zoptero.com@inbox.lv>
2026-04-28 11:05:03 +0300

    Start

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  assetPrefix: isProduction ? "https://dashboard.shadcnuikit.com" : undefined,
   images: {
     remotePatterns: [
       {
