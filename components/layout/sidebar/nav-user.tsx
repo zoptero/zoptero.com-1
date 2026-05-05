5b1b900d4186b89492f067ca9bbc4a0f5d1f2cbb
zoptero <zoptero.com@inbox.lv>
2026-05-05 06:31:16 +0300

    Clerk mobile login z index fix

--- components/layout/sidebar/nav-user.tsx
+++ components/layout/sidebar/nav-user.tsx
@@ -20,7 +20,7 @@ export function NavUser() {
               avatarBox: "h-8 w-8 rounded-lg",
               userButton: "flex items-center gap-2 h-10",
               userButtonTrigger: "flex items-center gap-2 h-10",
-              userButtonPopover: "w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg",
+              userButtonPopover: "w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg z-50",
               userMenuActions: "flex flex-col gap-1",
               userMenuItem: "flex items-center gap-2 px-2 py-1.5 text-sm",
               userMenuItemIcon: "h-4 w-4",
