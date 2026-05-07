5efff902836d1db59f8ba7f5423ef0a147b6a74c
zoptero <zoptero.com@inbox.lv>
2026-05-07 10:47:31 +0300

    Logo size change to 24 x 24 px

--- components/layout/sidebar/app-sidebar.tsx
+++ components/layout/sidebar/app-sidebar.tsx
@@ -42,7 +42,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
            <SidebarMenuItem>
               <SidebarMenuButton asChild className="hover:text-foreground h-10 group-data-[collapsible=icon]:px-0!">
                 <Link href="/dashboard">
-                  <img src="https://media.zoptero.com/img/zoptero-logo-32x32.svg" alt="Zoptero Logo" width={32} height={32} className="mr-2" />
+                  <img src="https://media.zoptero.com/img/zoptero-logo-32x32.svg" alt="Zoptero Logo" width={24} height={24} className="mr-2" />
                   <span className="text-foreground font-semibold">Zoptero</span>
                   <ChevronsUpDown className="ml-auto group-data-[collapsible=icon]:hidden" />
                 </Link>
