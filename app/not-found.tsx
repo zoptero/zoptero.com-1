fe331ca21d548065bc667627db3c3c16bec472cb
zoptero <zoptero.com@inbox.lv>
2026-05-07 21:37:21 +0300

    404 page creation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,37 +1,24 @@
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 export default function NotFound() {
   return (
-    <div className="bg-background grid h-screen items-center pb-8 lg:grid-cols-2 lg:pb-0">
-      <div className="text-center">
-        <p className="text-muted-foreground text-base font-semibold">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight md:text-5xl lg:text-7xl">
-          Page not found
-        </h1>
-        <p className="text-muted-foreground mt-6 text-base leading-7">
-          Sorry, we couldn’t find the page you’re looking for.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-2">
-          <Button size="lg" asChild>
-            <Link href="/dashboard">Go back home</Link>
-          </Button>
-          <Button size="lg" variant="ghost">
-            Contact support <ArrowRight className="ms-2 h-4 w-4" />
-          </Button>
-        </div>
-      </div>
-      <div className="hidden lg:block">
-        <img
-          src={`/404.svg`}
-          width={300}
-          height={400}
-          className="w-full object-contain lg:max-w-2xl"
-          alt="not found image"
-        />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-background px-4">
+      <Badge className="gap-1 mb-4" variant="outline">
+        <svg aria-hidden="true" className="text-emerald-600" width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.00016 11.1998L2.80016 7.99984L1.86683 8.93317L6.00016 13.0665L15.0002 4.0665L14.0668 3.13317L6.00016 11.1998Z" fill="currentColor"/></svg>
+        Informācijas platforma
+      </Badge>
+      <h1 className="text-center font-bold tracking-tight text-3xl md:text-5xl lg:text-7xl mb-4">404</h1>
+      <p className="text-center text-muted-foreground mb-8 max-w-md text-[12px]">Atvaino, šī lapa neeksistē vai ir pārvietota.</p>
+      <div className="flex flex-col sm:flex-row gap-2">
+        <Button size="lg" asChild>
+          <Link href="/dashboard">Doties uz sākumu</Link>
+        </Button>
+        <Button size="lg" variant="ghost">
+          Sazināties ar atbalstu <ArrowRight className="ms-2 h-4 w-4" />
+        </Button>
       </div>
     </div>
   );
